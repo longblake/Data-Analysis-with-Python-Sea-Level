06777ebe29d1b36f4f97f679c6542b9704725b3e
longblake <long.blake.k@gmail.com>
2025-03-17 02:03:03 +0000

    9pm 3-16-25

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,16 +4,26 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
+    df = pd.read_csv('epa-sea-level.csv')
 
 
     # Create scatter plot
+    plt.scatter(x=df.iloc[:, 0], y=df.iloc[:, 1])
+
+    # Add labels and title
+    plt.xlabel('Year')  # Label for x-axis using the column name
+    plt.ylabel('Sea Level (inches)')  # Label for y-axis using the column name
+    plt.title('Rise in Sea Level')
+    
+    #Year,CSIRO Adjusted Sea Level,Lower Error Bound,Upper Error Bound,NOAA Adjusted Sea Level
 
 
     # Create first line of best fit
-
+    line_1 = linregress(x=df.iloc[:, 0], y=df.iloc[:, 1])
+    plt.plot(df.iloc[:, 0],line_1.slope * df.iloc[:,0] + line_1.intercept)
 
     # Create second line of best fit
-
+    #linregress(x, y=None, alternative='two-sided')
 
     # Add labels and title
 
